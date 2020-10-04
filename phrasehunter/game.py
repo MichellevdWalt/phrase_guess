46e14c6e79e230e532ec9ce996c53d9dcb465cc2
MichellevdWalt <michellevdwalt13@gmail.com>
2020-10-04 10:11:07 +0200

    game update

--- phrasehunter/game.py
+++ phrasehunter/game.py
@@ -1,32 +1,71 @@
+import random
+
 import phrase
 import phrases
 print(phrases.phrases)
-# class Game:
 
-#     def __init__(self, missed=0, phrases, active_phrase = None, guesses=[] ):
-#         self.missed = missed
-#         self.phrases = phrases
-#         self.active_phrase = active_phrase
-#         self.guesses = guesses
+class Game:
+    # phrases = phrases.phrases
+    name = None
+    def __init__(self, phrases, missed=0, active_phrase = None, guesses=[], current_guess=None):
+        self.missed = missed
+        self.phrases = phrases
+        self.active_phrase = active_phrase
+        self.guesses = guesses
+        self.current_guess = current_guess
 
-#     def start():
-#         #start(): Calls the welcome method, creates the game loop, calls the get_guess method,
-#         #  adds the user's guess to guesses, increments the number of missed by one if the guess is incorrect,
-#         #  calls the game_over method.
-#         pass
+    def start(self):
+        self.welcome()
+        self.get_random_phrase()
+        start_phrase = phrase.Phrase(self.active_phrase)
+        start_phrase.display(self.name)
+        blanks = start_phrase.display(self.name)
+        self.current_guess = "".join(blanks)
+        while True:
+            self.get_guess()
+            current_guessed_letter = self.guesses[-1]
+            correct = start_phrase.check_letter(self.name, current_guessed_letter)
+            if correct:
+                index = []
+                number = -1
+                active_phrase = self.active_phrase
+                for letter in active_phrase:
+                    number += 1
+                    if letter == current_guessed_letter.lower():
+                        blanks[number] = current_guessed_letter
+                            
+                self.current_guess = " ".join(blanks)
+                complete = start_phrase.check_complete(self.current_guess)
+                print(complete)
+                if complete:
+                    print("Congratulations {}!!!! You guessed the phrase!!\n".format(self.name))
+                    print('"' + self.active_phrase + '"')
+                    break
+                else:
+                    print(self.current_guess)
+            
 
-#     def get_random_phrase():
-#         # this method randomly retrieves one of the phrases stored in the phrases list and returns it.
-#         pass
+        #start(): Calls the welcome method, creates the game loop, calls the get_guess method,
+        #  adds the user's guess to guesses, increments the number of missed by one if the guess is incorrect,
+        #  calls the game_over method.
 
-#     def welcome():
-#         # welcome(): this method prints a friendly welcome message to the user at the start of the game
-#         pass
+    def get_random_phrase(self):
+        number = random. randint(0,len(self.phrases)-1)
+        self.active_phrase = self.phrases[number]
 
-#     def get_guess():
-#         # this method gets the guess from a user and records it in the guesses attribute
-#         pass
+    def welcome(self):
+        print("Welcome to our phrase guessing game!")
+        name = input("What is your name?  ")
+        self.name = name
 
-#     def game_over():
-#         # game_over(): this method displays a friendly win or loss message and ends the game.
-#         pass
+    def get_guess(self):
+        guess = input("{}, please guess a letter from a-z  ".format(self.name))
+        self.guesses.append(guess)
+
+    def game_over(self):
+        # game_over(): this method displays a friendly win or loss message and ends the game.
+        pass
+
+
+new_game = Game(phrases.phrases)
+new_game.start()
--- phrasehunter/phrase.py
+++ phrasehunter/phrase.py
@@ -8,67 +8,60 @@ class Phrase:
         self.phrase = phrase.lower()
 
 
-    def display(self):
+    def display(self, name):
         phrase = list(self.phrase)
         blanks = []
-        print(phrase)
         for letter in phrase:
             if letter != " ":
                 blanks.append("_")
             elif letter == " ":
                 blanks.append(" ")
-        self.current_guess = " ".join(blanks)
-        print(self.current_guess)
+        print(" ".join(blanks))
+        return blanks
         
 
-        while True:
-            complete = self.check_complete()
-            print(complete)
-            if complete:
-                print("Congratulations!!!! You guessed the phrase")
-                break
-            else:
-                guess = self.check_letter()
-                print("Guess:" + guess)
-                index = []
-                start = 0
-                for letter in phrase:
-                    if letter == guess.lower():
-                        if start != 0:
-                            blanks[phrase.index(letter, start + 1)] = guess
-                            start = phrase.index(letter, start + 1)
-                        else:
-                            blanks[phrase.index(letter)] = guess
-                            start = phrase.index(letter)
+        # while True:
+        #     complete = self.check_complete()
+        #     print(complete)
+        #     if complete:
+        #         print("Congratulations {}!!!! You guessed the phrase".format(name))
+        #         break
+        #     else:
+        #         guess = self.check_letter(name)
+        #         print("Guess:" + guess)
+        #         index = []
+        #         start = 0
+        #         for letter in phrase:
+        #             if letter == guess.lower():
+        #                 if start != 0:
+        #                     blanks[phrase.index(letter, start + 1)] = guess
+        #                     start = phrase.index(letter, start + 1)
+        #                 else:
+        #                     blanks[phrase.index(letter)] = guess
+        #                     start = phrase.index(letter)
                             
-                self.current_guess = " ".join(blanks)
-                print(self.current_guess)
+        #         self.current_guess = " ".join(blanks)
 
 
-    def check_letter(self):
-        guess = None
-        while True:
-            correct = False
-            guess = input("Please guess a letter:  ")
-            phrase = self.phrase
-            print(phrase)
+    def check_letter(self, name, guess):
+        correct = False
+        phrase = self.phrase
 
-            for letter in phrase:
-                if letter == guess.lower():
-                    correct = True
+        for letter in phrase:
+            if letter == guess.lower():
+                correct = True
 
-            if correct:
-                print("You guessed right!")
-                break     
-            else:
-                print("Nope, try again!")
+        if correct:
+            print("You guessed right!")     
+        else:
+            print("Nope, try again!")
 
-        return guess
+        return correct
 
 
-    def check_complete(self):
+    def check_complete(self, current_guess):
         complete = False
-        if "_" in self.current_guess:
+        if "_" in current_guess:
             return False
         else:
             return True
