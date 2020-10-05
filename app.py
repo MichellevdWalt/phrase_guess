f387c8c77bdfe3ce3f3f023e201f01e8cc1a745f
MichellevdWalt <michellevdwalt13@gmail.com>
2020-10-05 15:45:35 +0200

    Final

--- app.py
+++ app.py
@@ -3,22 +3,39 @@ from phrasehunter.phrases import phrases
 
 if __name__ == "__main__":
     name = None
+    score = 0
+    games = 0
     while True:
         if name == None:
-            new_game = Game(phrases, None)
+            new_game = Game(phrases, score, games, None)
             again = new_game.start()
             if again[0]:
                 name = again[1]
+                games += 1
+                if again[2]:
+                    score +=1
                 continue    
             else:
+                name = again[1]
                 print("\nThanks for playing {}".format(name))
+                if again[2]:
+                    score += 1
+                games += 1
+                print("\nYour final score was {} out of {}".format(score, games))
                 break
         else:
-            new_game = Game(phrases, name)
+            new_game = Game(phrases, score, games, name)
             again = new_game.start()
             if again[0]:
+                if again[2]:
+                    score +=1
+                games += 1
                 continue
             else:
-                print("Thanks for playing {}".format(name))
+                print("\nThanks for playing {}".format(name))
+                if again[2]:
+                    score += 1
+                games += 1
+                print("\nYour final score was {} out of {}".format(score, games))
                 break
         
--- phrasehunter/game.py
+++ phrasehunter/game.py
@@ -3,13 +3,16 @@ from .phrase import Phrase
 # from .phrases import phrases
 
 class Game:
-    def __init__(self, phrases, name=None, missed=0, active_phrase=None, guesses=[], current_guess=None):
-        self.missed = missed
-        self.name = name
+    def __init__(self, phrases, score, games, name=None, missed=0, active_phrase=None, guesses=[], current_guess=None):
         self.phrases = phrases
+        self.score = score
+        self.games = games
+        self.name = name
+        self.missed = missed
         self.active_phrase = active_phrase
         self.guesses = guesses
         self.current_guess = current_guess
+
 
     def start(self):
         self.welcome()
@@ -20,7 +23,8 @@ class Game:
 
         while True:
             self.get_guess()
-            current_guessed_letter = self.guesses[-1]
+            guesses_len = len(self.guesses)
+            current_guessed_letter = self.guesses[guesses_len - 1]
             correct = start_phrase.check_letter(self.name, current_guessed_letter, self.missed)
             if correct:
                 index = []
@@ -43,10 +47,10 @@ class Game:
                     again = self.game_over(False)
                     break
 
-        if again:
-            return True, self.name
-        elif again == False:
-            return False, self.name
+        if again[0]:
+            return True, self.name, again[1]
+        elif again[0] == False:
+            return False, self.name, again[1]
 
 
     def get_random_phrase(self):
@@ -60,23 +64,43 @@ class Game:
             name = input("What is your name?  ")
             self.name = name
         else:
-            print("Welcome to another round {}".format(self.name))
+            print("\nWelcome to another round {} \n\nYour current score is {} out of {} games".format(self.name, self.score, self.games))
 
 
     def get_guess(self):
-        guess = input("\n{}, please guess a letter from a-z  ".format(self.name))
-        self.guesses.append(guess)
+        repeated = False
+        while True:
+            guess = input("\n{}, please guess a letter from a-z  ".format(self.name))
+            if guess.isalpha():
+                for letter in self.guesses:
+                    if letter.lower() == guess.lower():
+                        repeated = True                        
+                if repeated:
+                    print("\nOh-Oh...You guessed that one already. \nBe careful, this might cost a life if incorrect!")
+                    self.guesses.append(guess)
+                else:
+                    self.guesses.append(guess)
+                break
+            else:
+                print("\nThat doesn't seem to be a valid letter from a-z. Please try again.\n\nDon't worry, no life was taken")
+                continue
+            
 
 
     def game_over(self, win):
         if win:
             print("Congratulations {}!!!! You guessed the phrase!!\n".format(self.name))
             print('The correct phrase was: "' + self.active_phrase + '"\n')
-            again = input("Would you like to play again? y/n ")
         else:
             print("Better luck next time!!\n")
+        while True:
             again = input("Would you like to play again? y/n  ")
-        if again.lower() == "y":
-            return True
-        elif again.lower() == "n":
-            return False
+            if again.lower() == "y":
+                return True, win
+                break
+            elif again.lower() == "n":
+                return False, win
+                break
+            else:
+                print("\nPlease type only a 'y' or 'n'\n")
+                continue
